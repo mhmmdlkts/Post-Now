fb1ecd53f24e9c03e94078140d2b93b833ce01c6
Muhammed Ali Aktaş <mhmmdlkts@gmail.com>
2020-07-02 23:12:43 +0200

    IOS phone auth issue fixed

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,9 +1,11 @@
 import UIKit
 import Flutter
 import Firebase
+import FirebaseAuth
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -13,5 +15,18 @@ import Firebase
         FirebaseApp.configure()
     }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+             let firebaseAuth = Auth.auth()
+             firebaseAuth.setAPNSToken(deviceToken, type: AuthAPNSTokenType.unknown)
+   }
+
+   override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+             let firebaseAuth = Auth.auth()
+             if (firebaseAuth.canHandleNotification(userInfo)){
+                 print(userInfo)
+                 return
+             }
   }
 }
